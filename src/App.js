b2cc95b538d2c5595bc5902149eeaceb37e43f0f
wuwei <418060258@qq.com>
2022-01-07 17:52:18 +0800

    npm run eject

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "antd-mobile";
+import "./App.css";
+import "antd-mobile/es/global";
+import './sass/main.scss'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button color="primary">这是一个按钮a</Button>
+
+      <p>这是一段话</p>
     </div>
   );
 }
